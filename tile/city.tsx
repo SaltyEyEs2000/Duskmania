44cbcb56fb6bc34404f763704e8c27a7bd2c1b4c
V Ven <liwyyz@gmail.com>
2024-03-05 22:37:04 +0800

    示例地图

---
+++ tile/city.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="city" tilewidth="120" tileheight="140" spacing="2" margin="2" tilecount="112" columns="8">
+ <image source="../pic_source/kenney_hexagon-pack/Spritesheets/hexagonTerrain_sheet.png" width="1024" height="2048"/>
+</tileset>
---
+++ tile/duskmania.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="duskmania" tilewidth="120" tileheight="140" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="120" height="140" source="../pic_source/kenney_hexagon-pack/PNG/Tiles/Terrain/Dirt/dirt_06.png"/>
+ </tile>
+ <tile id="1">
+  <image width="120" height="140" source="../pic_source/kenney_hexagon-pack/PNG/Tiles/Terrain/Grass/grass_05.png"/>
+ </tile>
+ <tile id="2">
+  <image width="120" height="140" source="../pic_source/kenney_hexagon-pack/PNG/Tiles/Terrain/Mars/mars_07.png"/>
+ </tile>
+ <tile id="3">
+  <image width="120" height="140" source="../pic_source/kenney_hexagon-pack/PNG/Tiles/Terrain/Sand/sand_07.png"/>
+ </tile>
+ <tile id="4">
+  <image width="120" height="140" source="../pic_source/kenney_hexagon-pack/PNG/Tiles/Terrain/Stone/stone_07.png"/>
+ </tile>
+</tileset>
