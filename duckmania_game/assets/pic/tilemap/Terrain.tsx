3fa2fd9f9281204fc7d55f346d3b7069a6691958
V Ven <liwyyz@gmail.com>
2024-04-01 21:18:47 +0800

    资源导入

--- duckmania_game/assets/pic/tilemap/Terrain.tsx
+++ duckmania_game/assets/pic/tilemap/Terrain.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Terrain" tilewidth="88" tileheight="102" tilecount="12" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="Terrain" tilewidth="4015" tileheight="2180" tilecount="71" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="88" height="102" source="sea.png"/>
@@ -22,6 +22,9 @@
  <tile id="6">
   <image width="88" height="102" source="1000+.png"/>
  </tile>
+ <tile id="95">
+  <image width="4015" height="2180" source="../world/Chimaera_World_Map.png"/>
+ </tile>
  <tile id="13">
   <image width="88" height="102" source="country.png"/>
  </tile>
@@ -36,6 +39,183 @@
  </tile>
  <tile id="28">
   <image width="88" height="102" source="Capital2.png"/>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="tag" value="Nuland_Federation"/>
+  </properties>
+  <image width="88" height="102" source="country_tag/Nuland_Federation.png"/>
+ </tile>
+ <tile id="35">
+  <image width="88" height="102" source="country_tag/Coobanos.png"/>
+ </tile>
+ <tile id="36">
+  <image width="88" height="102" source="country_tag/Tabascona.png"/>
+ </tile>
+ <tile id="37">
+  <image width="88" height="102" source="country_tag/Angesturas.png"/>
+ </tile>
+ <tile id="38">
+  <image width="88" height="102" source="country_tag/United_Chiefdom_States_of_Tomoyo_Emirate.png"/>
+ </tile>
+ <tile id="39">
+  <image width="88" height="102" source="country_tag/State_of_Ennlake.png"/>
+ </tile>
+ <tile id="40">
+  <image width="88" height="102" source="country_tag/Kornorix_Republic.png"/>
+ </tile>
+ <tile id="42">
+  <image width="88" height="102" source="country_tag/Lonoys_Island.png"/>
+ </tile>
+ <tile id="43">
+  <image width="88" height="102" source="country_tag/United_Further_East.png"/>
+ </tile>
+ <tile id="44">
+  <image width="88" height="102" source="country_tag/Wu'yuan.png"/>
+ </tile>
+ <tile id="45">
+  <image width="88" height="102" source="country_tag/Auleutian.png"/>
+ </tile>
+ <tile id="46">
+  <image width="88" height="102" source="country_tag/Hong'you.png"/>
+ </tile>
+ <tile id="47">
+  <image width="88" height="102" source="country_tag/Wu'long.png"/>
+ </tile>
+ <tile id="48">
+  <image width="88" height="102" source="country_tag/Wang'guan.png"/>
+ </tile>
+ <tile id="49">
+  <image width="88" height="102" source="country_tag/Zhong'jiao.png"/>
+ </tile>
+ <tile id="50">
+  <image width="88" height="102" source="country_tag/Shi'cheng.png"/>
+ </tile>
+ <tile id="51">
+  <image width="88" height="102" source="country_tag/Qiao'jiang.png"/>
+ </tile>
+ <tile id="52">
+  <image width="88" height="102" source="country_tag/Lei'zhou.png"/>
+ </tile>
+ <tile id="53">
+  <image width="88" height="102" source="country_tag/Unovistan.png"/>
+ </tile>
+ <tile id="54">
+  <image width="88" height="102" source="country_tag/Vankor.png"/>
+ </tile>
+ <tile id="55">
+  <image width="88" height="102" source="country_tag/Ballera_Free_State.png"/>
+ </tile>
+ <tile id="56">
+  <image width="88" height="102" source="country_tag/N.B.P.R.png"/>
+ </tile>
+ <tile id="57">
+  <image width="88" height="102" source="country_tag/Tyumen_Republic.png"/>
+ </tile>
+ <tile id="59">
+  <image width="88" height="102" source="country_tag/Veramya.png"/>
+ </tile>
+ <tile id="60">
+  <image width="88" height="102" source="country_tag/Kazan.png"/>
+ </tile>
+ <tile id="61">
+  <image width="88" height="102" source="country_tag/Tula.png"/>
+ </tile>
+ <tile id="62">
+  <image width="88" height="102" source="country_tag/Vatula.png"/>
+ </tile>
+ <tile id="63">
+  <image width="88" height="102" source="country_tag/Olosk.png"/>
+ </tile>
+ <tile id="64">
+  <image width="88" height="102" source="country_tag/Golyok.png"/>
+ </tile>
+ <tile id="65">
+  <image width="88" height="102" source="country_tag/Uran.png"/>
+ </tile>
+ <tile id="66">
+  <image width="88" height="102" source="country_tag/Greater_Siegerkranz_Reich.png"/>
+ </tile>
+ <tile id="67">
+  <image width="88" height="102" source="country_tag/R.K.Column.png"/>
+ </tile>
+ <tile id="68">
+  <image width="88" height="102" source="country_tag/Kalmania.png"/>
+ </tile>
+ <tile id="69">
+  <image width="88" height="102" source="country_tag/Nordenland.png"/>
+ </tile>
+ <tile id="70">
+  <image width="88" height="102" source="country_tag/R.K.Wustan_und_Saxum.png"/>
+ </tile>
+ <tile id="71">
+  <image width="88" height="102" source="country_tag/Ivorica.png"/>
+ </tile>
+ <tile id="72">
+  <image width="88" height="102" source="country_tag/Uballitia.png"/>
+ </tile>
+ <tile id="73">
+  <image width="88" height="102" source="country_tag/Republik_Lokhatskaya.png"/>
+ </tile>
+ <tile id="75">
+  <image width="88" height="102" source="country_tag/R.K.Baquillat.png"/>
+ </tile>
+ <tile id="76">
+  <image width="88" height="102" source="country_tag/Tulandot.png"/>
+ </tile>
+ <tile id="77">
+  <image width="88" height="102" source="country_tag/Mohiakhan.png"/>
+ </tile>
+ <tile id="78">
+  <image width="88" height="102" source="country_tag/R.K.Gotnland.png"/>
+ </tile>
+ <tile id="79">
+  <image width="88" height="102" source="country_tag/Hypernord_Kolonie.png"/>
+ </tile>
+ <tile id="80">
+  <image width="88" height="102" source="country_tag/Mohenzo.png"/>
+ </tile>
+ <tile id="81">
+  <image width="88" height="102" source="country_tag/Kurvata.png"/>
+ </tile>
+ <tile id="82">
+  <image width="88" height="102" source="country_tag/Hjalmar.png"/>
+ </tile>
+ <tile id="83">
+  <image width="88" height="102" source="country_tag/Messier.png"/>
+ </tile>
+ <tile id="84">
+  <image width="88" height="102" source="country_tag/Commonwealth_of_West_Fragnos.png"/>
+ </tile>
+ <tile id="85">
+  <image width="88" height="102" source="country_tag/Kingdom_of_East_Fragnos.png"/>
+ </tile>
+ <tile id="86">
+  <image width="88" height="102" source="country_tag/Fragnos_Konorix_Clique.png"/>
+ </tile>
+ <tile id="87">
+  <image width="88" height="102" source="country_tag/Virreinato_Parathius.png"/>
+ </tile>
+ <tile id="88">
+  <image width="88" height="102" source="country_tag/Free_Island_of_Ellsworth.png"/>
+ </tile>
+ <tile id="89">
+  <image width="88" height="102" source="country_tag/Albejons_deuxième_République.png"/>
+ </tile>
+ <tile id="90">
+  <image width="88" height="102" source="country_tag/Kingdom_of_Aotearoa.png"/>
+ </tile>
+ <tile id="91">
+  <image width="88" height="102" source="country_tag/Kingdom_of_Wilkes_Island.png"/>
+ </tile>
+ <tile id="92">
+  <image width="88" height="102" source="country_tag/Union_of_Grand_Santarrtic.png"/>
+ </tile>
+ <tile id="93">
+  <image width="88" height="102" source="country_tag/North_Island_Union.png"/>
+ </tile>
+ <tile id="94">
+  <image width="88" height="102" source="country_tag/Kalecax_Island.png"/>
  </tile>
  <wangsets>
   <wangset name="未命名集" type="mixed" tile="-1">
