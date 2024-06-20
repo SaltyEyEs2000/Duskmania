c80bf31ad35e03970466a5f37a56fa97c192d8f5
king_v_ven <liww@kingnet.com>
2024-06-20 17:23:15 +0800

    单位数据

--- duckmania_game/assets/script/Data/Block_Data.ts
+++ duckmania_game/assets/script/Data/Block_Data.ts
@@ -5,3 +5,10 @@ export interface Block_Data{
     height:number;
     type:number;
 }
+export interface Battle_Unit_Data{
+    unit_type:number;
+    belong:number;
+    hp:number;
+    morale:number;
+    form:number;
+}
